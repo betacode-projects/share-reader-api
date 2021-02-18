a98f9d0ef9100e747a39cfcb6968836e42ada257
hirosuke-net <31305419+betacode-projects@users.noreply.github.com>
2021-02-18 15:07:00 +0900

    ダウンロードファイル確認

--- check_file.php
+++ check_file.php
@@ -5,7 +5,13 @@ require_once './__default.php';
 $json_list['data'] = ['flag' => false];
 
 
-// ----- DBへ登録
+// ----- トークンチェック
+if (!isset($_POST['token']) || strlen($_POST['token']) !== 64){
+    show_errors($json_list, 'トークン形式が正しくありません。');
+}
+
+
+// ----- DBへアクセス
 $link = @mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
 if (!$link) {
     show_errors($json_list, 'データベースからの応答がありません。しばらくたってからアクセスしてください。', 'DB');
@@ -13,4 +19,15 @@ if (!$link) {
 mysqli_set_charset($link, 'utf8');
 
 
-//TODO: トークンチェック
+$check_sql = 'SELECT * FROM file_download WHERE recv_token = "'. esc($link, $_POST['token']) .'"';
+$file_list = get_allrows($link, $check_sql);
+
+if (count($file_list) <= 0){
+    show_errors($json_list,'トークンと一致するファイルは見つかりませんでした。');
+}
+$sender_token = $file_list[0]['send_token'];
+
+
+// ----- json出力
+$json_list['data'] = ['token' => $sender_token];
+show_success($json_list, 'ファイルが見つかりました。');
